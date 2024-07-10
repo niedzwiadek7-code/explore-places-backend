8016b3f129433919fbf2b382617fbf1897d3736e
Damian Kliber <damiankliber@gmail.com>
2024-07-10 07:44:04 +0200

    feat: add cors

--- travel_app_backend/settings.py
+++ travel_app_backend/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'activities'
+    'activities',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -74,6 +75,7 @@ AUTH_USER_MODEL = 'activities.User'
 
 # Middleware settings
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -81,6 +83,18 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Cors settings
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
 ]
 
 ROOT_URLCONF = 'travel_app_backend.urls'
