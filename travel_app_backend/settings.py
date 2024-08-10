01e74ce3120c199a3ec8bc2901a1f0e98b2441a8
Damian Kliber <damiankliber@gmail.com>
2024-08-10 17:33:35 +0200

    feat: deploy app to vercel

--- travel_app_backend/settings.py
+++ travel_app_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
