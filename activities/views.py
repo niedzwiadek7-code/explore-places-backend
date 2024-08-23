0cd9ba034d0d1505b30fc457a6efc80a397616a4
Damian Kliber <damiankliber@gmail.com>
2024-08-23 14:46:16 +0200

    feat: #25 create first version of get activities endpoint

--- activities/views.py
+++ activities/views.py
@@ -1,3 +1,5 @@
+from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.geos import Point
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import JSONParser
@@ -34,12 +36,25 @@ def unlike_activity(request, activity_id):
     return Response({'message': 'Activity unliked'}, status=status.HTTP_200_OK)
 
 
-@api_view(['GET'])
+@api_view(['POST'])
+@parser_classes((JSONParser,))
 def get_some_activities(request):
+    user_latitude = request.data.get('latitude')
+    user_longitude = request.data.get('longitude')
+
     count_to_get = int(request.query_params.get('count', 10))
+
     activities_viewed = ActivityView.objects.filter(user=request.user)
     activities_viewed_ids = [activity_view.activity.id for activity_view in activities_viewed]
-    activities = ActivityEntity.objects.exclude(id__in=activities_viewed_ids)[:count_to_get]
+
+    if user_latitude is None or user_longitude is None:
+        activities = ActivityEntity.objects.exclude(id__in=activities_viewed_ids).order_by('?')[:count_to_get]
+    else:
+        user_location = Point(float(user_longitude), float(user_latitude), srid=4326)
+
+        activities = ActivityEntity.objects.exclude(id__in=activities_viewed_ids).annotate(
+            distance=Distance('point_field', user_location)
+        ).order_by('distance')[:count_to_get]
 
     serializer = ActivitySerializer(activities, many=True, context={'request': request})
     return Response(serializer.data, status=status.HTTP_200_OK)
