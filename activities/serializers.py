878f7adff0f96c01ed82faee421649ad34c9cff0
Damian Kliber <damiankliber@gmail.com>
2024-08-23 09:25:39 +0200

    feat: #25 improve serializer for new coordinates saving way

--- activities/serializers.py
+++ activities/serializers.py
@@ -16,6 +16,7 @@ class ExternalLinksSerializer(serializers.ModelSerializer):
 
 class ActivitySerializer(serializers.ModelSerializer):
     liked_by_user = serializers.SerializerMethodField()
+    coordinates = serializers.SerializerMethodField()
     address = AddressSerializer(read_only=True)
     external_links = ExternalLinksSerializer()
 
@@ -28,6 +29,14 @@ class ActivitySerializer(serializers.ModelSerializer):
         if user.is_anonymous:
             return False
         return ActivityLike.objects.filter(activity=obj, user=user).exists()
+
+    def get_coordinates(self, obj):
+        if obj.point_field:
+            return dict(
+                latitude=obj.point_field.coords[1],
+                longitude=obj.point_field.coords[0]
+            )
+        return None
 
 
 class ActivityLikeSerializer(serializers.ModelSerializer):
