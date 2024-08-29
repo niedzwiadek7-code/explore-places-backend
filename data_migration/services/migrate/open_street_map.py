099d55c4e10428467ca5215f9a514200fbe3755c
Damian Kliber <damiankliber@gmail.com>
2024-08-29 08:21:41 +0200

    feat: improve fetchData fn to return data instantly after fetching

--- data_migration/services/migrate/open_street_map.py
+++ data_migration/services/migrate/open_street_map.py
@@ -24,69 +24,67 @@ class OpenStreetMapMigrationService(DataMigrationService):
 
     async def fetch_data(self, args):
         min_lat = float(args.get('min_lat'))
-        max_lat = float(args.get('max_lat'))
+        max_lat = float(args.get('max_lat')) - 0.001
         min_lon = float(args.get('min_lon'))
-        max_lon = float(args.get('max_lon'))
+        max_lon = float(args.get('max_lon')) - 0.001
 
         step_lat = 0.1
         step_lon = 0.1
 
-        async def step(min_lat_loc, max_lat_loc, min_lon_loc, max_lon_loc):
+        async def step_was_processed(min_lat_loc, max_lat_loc, min_lon_loc, max_lon_loc):
             async for field in OpenTripMapServiceData.objects.filter(
                     min_latitude=min_lat_loc,
                     max_latitude=max_lat_loc,
                     min_longitude=min_lon_loc,
                     max_longitude=max_lon_loc
             ):
-                self.logger.info(f'Step with lat: {min_lat_loc}-{max_lat_loc} and lon: {min_lon_loc}-{max_lon_loc} was already processed')
-                return []
+                return True
 
-            places_result = self.api_service.request(
-                method='GET',
-                endpoint=f'/places/bbox',
-                query_params=dict(
-                    lon_min=min_lon_loc,
-                    lon_max=max_lon_loc,
-                    lat_min=min_lat_loc,
-                    lat_max=max_lat_loc,
-                    apikey=self.api_key,
-                    rate=3,
-                    kinds=f'interesting_places,amusements,adult,foods,transport,accomodations'.replace(',', '%2C')
-                )
-            )
+            return False
 
-            places_ids = list(map(lambda x: x.get('properties').get('xid'), places_result.get('features')))
-            self.logger.info(
-                f'Found {len(places_ids)} places for step with lat: {min_lat_loc}-{max_lat_loc} and lon: {min_lon_loc}-{max_lon_loc}'
-            )
+        for lat in np.arange(min_lat, max_lat, step_lat):
+            for lon in np.arange(min_lon, max_lon, step_lon):
+                min_lat_loc = round(lat, 1)
+                max_lat_loc = round(lat + step_lat, 1)
+                min_lon_loc = round(lon, 1)
+                max_lon_loc = round(lon + step_lon, 1)
 
-            async def fetch_place_data(place_id):
-                return self.api_service.request(
+                if await step_was_processed(min_lat_loc, max_lat_loc, min_lon_loc, max_lon_loc):
+                    self.logger.info(f'Step with lat: {min_lat_loc}-{max_lat_loc} and lon: {min_lon_loc}-{max_lon_loc} was already processed')
+                    continue
+
+                places_result = self.api_service.request(
                     method='GET',
-                    endpoint=f'/places/xid/{place_id}',
-                    query_params=dict(apikey=self.api_key)
+                    endpoint=f'/places/bbox',
+                    query_params=dict(
+                        lon_min=min_lon_loc,
+                        lon_max=max_lon_loc,
+                        lat_min=min_lat_loc,
+                        lat_max=max_lat_loc,
+                        apikey=self.api_key,
+                        rate=3,
+                        kinds=f'interesting_places,amusements,adult,foods,transport,accomodations'.replace(',', '%2C')
+                    )
                 )
 
-            place_tasks = [fetch_place_data(place_id) for place_id in places_ids]
+                places_ids = list(map(lambda x: x.get('properties').get('xid'), places_result.get('features')))
+                self.logger.info(
+                    f'Found {len(places_ids)} places for step with lat: {min_lat_loc}-{max_lat_loc} and lon: {min_lon_loc}-{max_lon_loc}'
+                )
 
-            await OpenTripMapServiceData.objects.acreate(
-                min_latitude=min_lat_loc,
-                max_latitude=max_lat_loc,
-                min_longitude=min_lon_loc,
-                max_longitude=max_lon_loc
-            )
+                for place_id in places_ids:
+                    yield self.api_service.request(
+                        method='GET',
+                        endpoint=f'/places/xid/{place_id}',
+                        query_params=dict(apikey=self.api_key)
+                    )
 
-            return await asyncio.gather(*place_tasks)
-
-        step_tasks = [
-            step(round(lat, 1), round(lat + step_lat, 1), round(lon, 1), round(lon + step_lon, 1))
-            for lat in np.arange(min_lat, max_lat, step_lat)
-            for lon in np.arange(min_lon, max_lon, step_lon)
-        ]
-
-        for step_result in await asyncio.gather(*step_tasks):
-            for data in step_result:
-                yield data
+                await OpenTripMapServiceData.objects.acreate(
+                    min_latitude=min_lat_loc,
+                    max_latitude=max_lat_loc,
+                    min_longitude=min_lon_loc,
+                    max_longitude=max_lon_loc
+                )
 
     @sync_to_async
     def process_data(self, data):
